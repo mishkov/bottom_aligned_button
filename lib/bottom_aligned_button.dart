7d8dda272d84f978903ecc927a97a7ed45c2af43
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-19 17:05:24 +0300

    Add BottomAlignedButton

--- lib/bottom_aligned_button.dart
+++ lib/bottom_aligned_button.dart
@@ -1,7 +1,52 @@
 library bottom_aligned_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+import 'distanced_widgets.dart';
+
+class BottomAlignedButton extends StatefulWidget {
+  const BottomAlignedButton({
+    required this.button,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget child;
+  final Widget button;
+
+  @override
+  State<BottomAlignedButton> createState() => _BottomAlignedButtonState();
+}
+
+class _BottomAlignedButtonState extends State<BottomAlignedButton> {
+  bool isButtonShowed = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return DistancedWidgets(
+      top: ListView(
+        physics: isButtonShowed ? const NeverScrollableScrollPhysics() : null,
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        children: [
+          widget.child,
+          Visibility(
+            visible: !isButtonShowed,
+            child: widget.button,
+          ),
+        ],
+      ),
+      bottom: widget.button,
+      onBottomHide: () {
+        setState(() {
+          isButtonShowed = false;
+        });
+      },
+      onBottomShow: () {
+        setState(() {
+          isButtonShowed = true;
+        });
+      },
+    );
+  }
 }
