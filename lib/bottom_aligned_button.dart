e2443e286c0089cfcfea7dca0bd36d98773613b7
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-19 21:22:53 +0300

    Add library-level doc comment

--- lib/bottom_aligned_button.dart
+++ lib/bottom_aligned_button.dart
@@ -1,3 +1,5 @@
+/// Provides [BottomAlignedButton] and [DistancedWidgets] widget that helps to
+/// place button (or any widget) at the bottom of the screen.
 library bottom_aligned_button;
 
 export 'src/bottom_aligned_button.dart';
